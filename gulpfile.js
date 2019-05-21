7d0dd2add391cb78e7b3096869881c1f41160324
Aleksandr Gulbickij <4efirr@gmail.com>
2019-05-21 16:03:47 +0300

    Using dart sass instead of ruby sass. Uploaded lint configuration

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 'use strict';
 
 var gulp = require('gulp');
-var sass = require('gulp-sass');
-var scsslint = require('gulp-scss-lint');
-var server = require('gulp-webserver');
+var sass = require('gulp-dart-sass');
+// var scsslint = require('gulp-scss-lint');
+// var server = require('gulp-webserver');
 
 
-sass.compiler = require('node-sass');
+// sass.compiler = require('node-sass');
 
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
@@ -14,19 +14,27 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./css'));
 });
 
-gulp.task('scss-lint', function() {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(scsslint());
-});
+gulp.task('lint-css', function lintCssTask() {
+  const gulpStylelint = require('gulp-stylelint');
 
-gulp.task('server', function() {
-  gulp.src('app')	// <-- your app folder
-    .pipe(server({
-      livereload: true,
-      open: true,
-      port: 6000	// set a port to avoid conflicts with other local apps
+  return gulp
+    .src('sass/**/*.scss')
+    configFile('.stylelintrc.yml')
+    .pipe(gulpStylelint({
+      reporters: [
+        {formatter: 'string', console: true}
+      ]
     }));
 });
+
+// gulp.task('server', function() {
+//   gulp.src('app')	// <-- your app folder
+//     .pipe(server({
+//       livereload: true,
+//       open: true,
+//       port: 6000	// set a port to avoid conflicts with other local apps
+//     }));
+// });
 
 gulp.task('watch', function() {
   gulp.watch('./sass/**/*.scss', gulp.series('sass'));
